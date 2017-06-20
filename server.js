920ea240ce9005e92755e824723eaefdb2572dfe
TEC\GP <Georg.Popp@tecalliance.net>
2017-06-20 16:00:15 +0200

    Added projects.hbs view. Added /projects route

--- server.js
+++ server.js
@@ -51,7 +51,13 @@ app.get('/about', (req, res) => {
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
-    })
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
 });
 
 app.listen(port, () => {
